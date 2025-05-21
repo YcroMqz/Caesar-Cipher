f5020c1ee0651d8605117c70aa083f1d90470ce7
YcroMqz <icayw12@gmail.com>
2025-05-21 00:05:22 -0300

    init commit

---
+++ caesarcipher.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    char sent[256];
+    unsigned int key;
+
+    printf("Sentence: ");
+    scanf("%256[^\n]s", sent);
+    getchar();
+
+    printf("Key: ");
+    scanf("%d", &key);
+
+    for(int i = 0; sent[i] != '\0'; i++){
+        char ch = sent[i];
+        if(isalpha(ch)){
+            char base = isupper(ch) ? 'A' : 'a';
+            sent[i] = base + (ch - base + key)%26;
+        }
+        printf("%c", sent[i]);
+    }
+    printf("\n");
+    getchar();
+
+    return 0;
+}
